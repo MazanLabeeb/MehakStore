9d7711426d2c56872ef461fb44b339ac8056e94d
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-04 15:24:21 +0500

    Lesson 63

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import "./categories.styles.scss";
-import CategoryItem from "./Components/category-item/category-item.component";
+import Directory from "./Components/directory/directory.component";
 
 const App = () => {
   const categories = [
@@ -31,15 +30,7 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem category = {category} key={category.id} />
-      )
-
-      )}
-
-
-    </div>
+    <Directory categories={categories} />
   );
 }
 
