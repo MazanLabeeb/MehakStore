6ca9b8163870e9e85a73aa914bf2f642579c5dcc
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-04 23:48:49 +0500

    Lesson 70

--- src/Components/PageNotFound/PageNotFound.component.jsx
+++ src/Components/PageNotFound/PageNotFound.component.jsx
@@ -2,7 +2,7 @@ import "./PageNotFound.style.scss";
 
 const PageNotFound = () => (
     <div className="PageNotFound">
-        <h1><i class="fa fa-chain-broken" aria-hidden="true"></i> 404!</h1>
+        <h1><i className="fa fa-chain-broken" aria-hidden="true"></i> 404!</h1>
         <p>Ooops! The Page you are looking for cannot be found.</p>
     </div>
 );
