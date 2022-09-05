a9c93abcf204fd8071e38d1d754af03b77cfd5ce
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-06 02:38:55 +0500

    Lesson 82

--- src/Components/form-input/form-input.component.jsx
+++ src/Components/form-input/form-input.component.jsx
@@ -1,10 +1,11 @@
+import "./form-input.style.scss";
 
 const FormInput = ({label, ...formProps}) => {
 
     return (
         <div>
             <label htmlFor={formProps.name}>{label}</label>
-            <input {...formProps} required />
+            <input {...formProps} className={"inputField"}   />
         </div>
     );
 }
--- src/Components/sign-up-form/sign-up-form.component.jsx
+++ src/Components/sign-up-form/sign-up-form.component.jsx
@@ -5,15 +5,15 @@ import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "
 import FormInput from "../form-input/form-input.component";
 
 const defaultFormFields = {
-    displayName: "", 
-    email: "", 
-    password:"", 
-    password2:""
+    displayName: "",
+    email: "",
+    password: "",
+    password2: ""
 }
 
-const SignUpForm = () =>{
+const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
-    const {displayName , email, password, password2} = formFields;
+    const { displayName, email, password, password2 } = formFields;
     // console.log(formFields);
 
     const formHandler = (event) => {
@@ -23,32 +23,41 @@ const SignUpForm = () =>{
         })
     }
 
-    const resetFormFields = () =>{
+    const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
 
     const submitHandler = async (event) => {
         event.preventDefault();
-        if(password != password2){
+        if (password != password2) {
             alert("Passwords do not match");
             return;
         }
 
-        try{
-            const {user} = await createAuthUserWithEmailAndPassword(
-                email, 
+        let submit = document.getElementById("submit");
+        submit.disabled = true;
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(
+                email,
                 password
             );
-            await createUserDocumentFromAuth(user, {displayName});
+            await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
-
-
-            console.log(user);
-        }catch( e ){
-            if(e.code === "auth/email-already-in-use"){
+            alert("Account created successfully!");
+        } catch (e) {
+            if (e.code === "auth/email-already-in-use") {
                 alert("Cannot create user, email is already in use");
+                submit.disabled = false;
+
+            } else if (e.code === "auth/weak-password") {
+                alert("Please choose a strong password!");
+                submit.disabled = false;
+
             }
             console.log("User creation encountered an error", e);
+            submit.disabled = false;
+
         }
     }
 
@@ -56,12 +65,12 @@ const SignUpForm = () =>{
         <div className="signup-form">
             <h3>Sign Up With Your Email and Password</h3>
             <form onSubmit={submitHandler}>
-                <FormInput type = {"text"} label={"Display Name"}   onChange={formHandler} value={displayName} name={"displayName"} />
-                <FormInput type = {"email"} label={"Email"}   onChange={formHandler} value={email} name={"email"} />
-                <FormInput type = {"password"} label={"password"}  onChange={formHandler} value={password} name={"password"} />
-                <FormInput type = {"password"} label={"password2"}   onChange={formHandler} value={password2} name={"password2"} />
+                <FormInput type={"text"} label={"Display Name"} onChange={formHandler} value={displayName} name={"displayName"} id={"displayName"} autoComplete="off" required={true} />
+                <FormInput type={"email"} label={"Email"} onChange={formHandler} value={email} name={"email"} id={"email"} autoComplete="off" required={true} />
+                <FormInput type={"password"} label={"Password"} onChange={formHandler} value={password} name={"password"} id={"password"} autoComplete="off" required={true} />
+                <FormInput type={"password"} label={"Repeat Password"} onChange={formHandler} value={password2} name={"password2"} id={"password2"} autoComplete="off" required={true} />
 
-                <button type="submit">Sign Up</button>
+                <button id="submit" type="submit">Sign Up</button>
             </form>
         </div>
     )
