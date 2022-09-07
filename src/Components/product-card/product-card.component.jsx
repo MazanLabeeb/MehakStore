9c49cde079c72c20acaa12d350df670ae1d7a0ac
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-07 23:46:59 +0500

    Lesson 96

--- src/Components/product-card/product-card.component.jsx
+++ src/Components/product-card/product-card.component.jsx
@@ -18,11 +18,3 @@ const ProductCard = ({product}) => {
 }
 
 export default ProductCard;
-
-
-// {
-//     "id": 1,
-//     "name": "Brown Brim",
-//     "imageUrl": "https://i.ibb.co/ZYW3VTp/brown-brim.png",
-//     "price": 25
-//   }
