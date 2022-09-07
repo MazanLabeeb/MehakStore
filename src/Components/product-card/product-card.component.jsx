62ccae075c9e9f11022f65a80d2e980faed766da
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-08 00:16:54 +0500

    Lesson 96

--- src/Components/product-card/product-card.component.jsx
+++ src/Components/product-card/product-card.component.jsx
@@ -8,10 +8,10 @@ const ProductCard = ({product}) => {
     return (
         <div className="product-card-container">
             <img src={imageUrl} alt={`Product ${name}`} />
+            <Button className="btn btn-secondary btn-large addCart">Add to Cart</Button>
             <div className="body">
                 <span className="name">{name}</span>
-                <span className="price">{price}</span>
-                <Button className="btn btn-secondary">ADD CART</Button>
+                <span className="price">{price}$</span>
             </div>
         </div>
     )
--- src/Components/shop/shop.component.jsx
+++ src/Components/shop/shop.component.jsx
@@ -1,19 +1,20 @@
 import { useContext } from "react";
 import { ProductsContext } from "../../Context/products.context";
 import ProductCard from "../product-card/product-card.component";
-
+import "../directory/directory.style.scss";
 
 
 
 const Shop = () => {
     const { products } = useContext(ProductsContext);
     return (
-        products.map(( product ) => {
-            console.log(product)
-            return (
-                <ProductCard key={product.id} product={product} /> 
-            );
-        })
+        <div className="categories-container">
+            {products.map((product) => {
+                return (
+                    <ProductCard key={product.id} product={product} />
+                );
+            })}
+        </div>
     );
 
 }
