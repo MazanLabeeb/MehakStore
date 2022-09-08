cdbf56ef1c33a68415e6992c4ae8efedcff086ce
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-08 23:20:42 +0500

    save

--- src/Context/products.context.jsx
+++ src/Context/products.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
     products : []
@@ -8,7 +8,7 @@ export const ProductsContext = createContext({
 
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
     const value = {products, setProducts};
 
 
