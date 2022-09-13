d316605d13b7a10e701d84ec5baeb19a1bea9ca2
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-13 18:49:11 +0500

    REDUCER ADDED TO CART CONTEXT

--- src/Context/cart.context.jsx
+++ src/Context/cart.context.jsx
@@ -1,56 +1,67 @@
-import { createContext, useEffect, useState } from "react";
+import { useReducer } from "react";
+import { createContext, useState } from "react";
 
 
 export const CartContext = createContext([]);
 
-const addItemsToCartHelper = ( cartItems, newCartItem ) => {
-    const newCartItemAlreadyExists = cartItems.find( row =>  row.id === newCartItem.id );
+const addItemsToCartHelper = (cartItems, newCartItem) => {
+    const newCartItemAlreadyExists = cartItems.find(row => row.id === newCartItem.id);
 
-    if(newCartItemAlreadyExists){
-        return cartItems.map( row => {
-            if(row.id === newCartItem.id){
+    if (newCartItemAlreadyExists) {
+        return cartItems.map(row => {
+            if (row.id === newCartItem.id) {
                 row.quantity++;
             }
             return row;
-        } )
+        })
     }
 
     newCartItem = { ...newCartItem, "quantity": 1 };
-    return [ ...cartItems, newCartItem ];
+    return [...cartItems, newCartItem];
+}
+
+const cartReducer = (currentState = [], action = {}) => {
+    const { type, payload } = action;
+    switch (type) {
+        case "ADD_ITEMS_TO_CART":
+            return payload
+            break;
+        default:
+            throw new Error("Unknown type has been passed to the cartReducer");
+    }
 }
 
 
+export const CartProvider = ({ children }) => {
+    // const [cartItems, setCartItems] = useState([]);
+    const [cartItems, dispatch] = useReducer(cartReducer, []);
 
-export const CartProvider = ({children}) => {
-    const [cartItems, setCartItems] = useState([]);
-
-    const addItemsToCart = ( newCartItem ) => {
-        setCartItems( addItemsToCartHelper(cartItems, newCartItem) );
+    const addItemsToCart = (newCartItem) => {
+        dispatch({type:"ADD_ITEMS_TO_CART", payload : addItemsToCartHelper(cartItems, newCartItem)});
     }
 
     const removeItemFromCart = (id) => {
-        setCartItems(cartItems.filter(cartItem=>cartItem.id != id));
-
+        dispatch({type:"ADD_ITEMS_TO_CART", payload : cartItems.filter(cartItem => cartItem.id != id)});
     }
 
     const incrementItemFromCart = (id) => {
-        setCartItems(cartItems.map( cartItem => {
-            if(cartItem.id == id)cartItem.quantity += 1;
+        dispatch({type:"ADD_ITEMS_TO_CART", payload : cartItems.map(cartItem => {
+            if (cartItem.id == id) cartItem.quantity += 1;
             return cartItem;
-        } ));
+        }) });
     }
 
     const decrementItemFromCart = (id) => {
-        setCartItems(cartItems.filter( cartItem => {
-            if(cartItem.id == id) cartItem.quantity -= 1;
-            if(cartItem.quantity > 0)return cartItem;
-        } ));
+        dispatch({type:"ADD_ITEMS_TO_CART", payload : cartItems.filter(cartItem => {
+            if (cartItem.id == id) cartItem.quantity -= 1;
+            if (cartItem.quantity > 0) return cartItem;
+        })});        
     }
 
-    
-    const value = {cartItems , setCartItems, addItemsToCart,removeItemFromCart, incrementItemFromCart, decrementItemFromCart};
 
-   
+    const value = { cartItems, dispatch, addItemsToCart, removeItemFromCart, incrementItemFromCart, decrementItemFromCart };
+
+
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
