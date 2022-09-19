1c2f14bbb6d900c44cbb38f6ae90f8fabe7b0945
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-19 13:27:10 +0500

    selector added

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,1 +1,8 @@
-export const categoriesMapSelector = (state=>state.categories);
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = state => state.categories;
+
+export const categoriesMapSelector = createSelector(
+    [selectCategoryReducer],
+    categories => categories
+)
