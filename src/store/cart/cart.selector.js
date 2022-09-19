2dc6e62e4cc8d8bfb34a3268905b9179124daf00
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-19 16:50:45 +0500

    146

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -1,1 +1,11 @@
-export const cartSelector = state => state.cart;
+import { createSelector } from "reselect";
+
+const cartSelectorState = state => state.cart;
+
+export const cartSelector = createSelector(
+    [cartSelectorState],
+    cart => {
+        console.log("triggered");
+        return cart
+    }
+)
