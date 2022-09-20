afb3c5243b5ef5b7a0fee0e8e3070413eed06dcb
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-20 18:51:14 +0500

    redux saga

--- src/store/store.js
+++ src/store/store.js
@@ -4,8 +4,12 @@ import { rootReducer } from "./root-reducer";
 import storage from "redux-persist/lib/storage";
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
+import createSagaMiddleware from "@redux-saga/core";
+import { rootSage } from "./root-saga";
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [process.env.NODE_ENV !== 'production' && logger , sagaMiddleware].filter(Boolean);
 
 const ChromeDevTools = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const composedEnhancer = ChromeDevTools(applyMiddleware(...middlewares));
@@ -17,4 +21,7 @@ const persistConfig = {
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = createStore(persistedReducer, undefined, undefined);  
+
+sagaMiddleware.run(rootSage);
+
 export const persistor = persistStore(store);
