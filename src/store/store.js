525b22502917def90578b998abbd71b68a976b96
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-20 20:00:19 +0500

    lesson 160

--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,9 @@ import { rootReducer } from "./root-reducer";
 import storage from "redux-persist/lib/storage";
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
-import createSagaMiddleware from "@redux-saga/core";
-import { rootSage } from "./root-saga";
 
-const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger , sagaMiddleware].filter(Boolean);
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
 
 const ChromeDevTools = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const composedEnhancer = ChromeDevTools(applyMiddleware(...middlewares));
@@ -20,8 +17,7 @@ const persistConfig = {
     blacklist: ["user", "categories"]
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(persistedReducer, undefined, undefined);  
+export const store = createStore(persistedReducer, undefined, undefined);    
 
-sagaMiddleware.run(rootSage);
 
 export const persistor = persistStore(store);
