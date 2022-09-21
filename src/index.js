5dea857ca34445955853b7c054f35943909a1b60
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-21 23:49:08 +0500

    lesson 162

--- src/index.js
+++ src/index.js
@@ -10,14 +10,21 @@ import { persistor, store } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor = {persistor}>
-      <BrowserRouter>
-              <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
