7a957b4da1f34f894bfb209f2de00e184fd0b2a7
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-22 00:14:21 +0500

    163

---
+++ src/Components/payment-form/payment-form.component.jsx
@@ -0,0 +1,25 @@
+
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import Button from "../button/button.component";
+
+const PaymentForm = () => {
+    const PaymentForm = () => {
+        const stripe = useStripe();
+        const elements = useElements();
+    }
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) return;
+    }
+
+    return (
+        <div>
+            <CardElement>
+                <Button className={"btn btn-primary"}>Pay Now</Button>
+            </CardElement>
+        </div>
+    );
+}
+
+export default PaymentForm;
