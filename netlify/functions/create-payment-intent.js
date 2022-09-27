accee2ad70ff5900763fc3db439c38b3ed8bd6f9
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-27 19:40:27 +0500

    162

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,26 +1,26 @@
-require("dotenv").config(); 
+require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-
 exports.handler = async (event) => {
-    try{
-        const { amount } = JSON.parse(event.body);
-        const paymentIntent = await stripe.paymentIntent.create({
-            amount, 
-            currency: 'usd',
-            payment_method_types: ["card"]
-        })
+  try {
+    const { amount } = JSON.parse(event.body);
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({paymentIntent})
-        }
-    }catch(error){
-        console.log({error});
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
 
-        return{
-            status : 400, 
-            body: JSON.stringify({error})
-        }
-    }
-}
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log({ error });
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
--- src/Components/payment-form/payment-form.component.jsx
+++ src/Components/payment-form/payment-form.component.jsx
@@ -3,25 +3,49 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import Button from "../button/button.component";
 
 const PaymentForm = () => {
-    const PaymentForm = () => {
-        const stripe = useStripe();
-        const elements = useElements();
+    const stripe = useStripe();
+    const elements = useElements();
 
-        const paymentHandler = async (e) => {
-            e.preventDefault();
-    
-            if (!stripe || !elements){ return;}
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) { return; }
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+        }).then(res => res.json());
+
+        const {paymentIntent:{client_secret}} = response;
+        console.log(client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method:{
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Mazan Labeeb'
+                }
+            }
+        });
+
+        if(paymentResult.error){
+            alert(paymentResult.error);
+
+        }else{
+            if(paymentResult.paymentIntent.status === "succeeded"){
+                alert("payment successful");
+            }
         }
-        
+
+
     }
-    
+
+
+
 
     return (
-        <div>
+        <div onClick={paymentHandler}>
             <CardElement>
-            {/* <Button className={"btn btn-primary"}>Pay Now</Button> */}
-                <button>Pay Now</button>
+                {/* <Button className={"btn btn-primary"}>Pay Now</Button> */}
             </CardElement>
+                <button>Pay Now</button>
         </div>
     );
 }
