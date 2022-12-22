28aecd67508114bbf624f28bf521d4f1e4fe4b40
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-12-23 00:00:48 +0500

    data.ts finalized

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,5 +1,4 @@
-
-export const getData = async (url:string) => {
+export const getData = async <T>(url:string):Promise<T> => {
     const response = await fetch(url);
     return response.json();
 }
